ca19a8a5a4bb0c0501494e57d4fce71a4b0ec644
Phineas Guo <31276761+guoPhineas@users.noreply.github.com>
2025-12-21 22:48:32 +0800

    Update: workflow

--- update.py
+++ update.py
@@ -6,13 +6,12 @@ delta=today-joinDate
 
 replace={
     "{days}":f'{delta.days}'
-    }
+}
 
 with open("./Readme.md","w+") as file:
     with open("./Readme_template.md","r") as template:
         templateContent=template.read()
         for i in replace:
-            
             newContent=templateContent.replace(i,replace[i])
         file.write(newContent)
 
